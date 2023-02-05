f26da4a8d75725642371e61cef14ea116973ea89
L <110619033+Learner062022@users.noreply.github.com>
2023-02-05 16:40:43 +0800

    completed exercise

---
+++ first-password-generator.py
@@ -0,0 +1,11 @@
+import string 
+import random
+characters_generator = string.ascii_lowercase + string.ascii_uppercase + string.punctuation
+tuple_numbers = tuple('0','1', '2', '3', '4', '5', '6', '7', '8', '9')
+for number in tuple_numbers:
+    characters_generator += number
+    password = ''
+    while len(password) != 10:
+        password += random.choice(characters_generator)
+    else:
+        print(password)
